f9c78758baa21f82a955bde1488f8364a5da4293
kshim1208 <a41533503@gmail.com>
2023-05-23 15:35:48 +0900

    checking ex02 now

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,11 +10,16 @@ int main( void )
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::cout << "----------------"<< std::endl;
+
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+
 	return 0;
 }
